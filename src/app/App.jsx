38c9e681bccf6548235d76f1967887f82e3d02cb
dandamdandam <glue0440@gmail.com>
2024-10-01 16:23:35 +0900

    chore: 디자인 기본 세팅

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,3 +1,6 @@
+import './global.css';
+import './declaration.d.ts';
+
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import styles from './index.module.css';
+
 export default function Main() {
-  return <div>main</div>;
+  return <div className={styles.main}>main</div>;
 }
