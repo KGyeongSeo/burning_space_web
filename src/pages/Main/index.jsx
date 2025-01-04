f05c03f92425e8f84f0ba8311f730022a7f0755e
dandamdandam <glue0440@gmail.com>
2025-01-04 21:57:41 +0900

    feat: 메인페이지 로고 구현

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
+import { useEffect } from 'react';
 
 import PROJECT from '/src/resources/project.json';
 
 import Carrosel from './Carrosel';
+import styles from './index.module.css';
 import ProjectCard from './ProjectCard';
 
+const handleScroll = (e) => {
+  if (scrollY < innerHeight - 50 && e.deltaY > 0) {
+    scrollTo({
+      top: innerHeight,
+      behavior: 'smooth',
+    });
+  }
+};
+
 export default function Main() {
+  useEffect(() => {
+    addEventListener('wheel', handleScroll);
+    return () => {
+      removeEventListener('wheel', handleScroll);
+    };
+  }, []);
   return (
     <div>
+      <header className={styles.header}>
+        <img src='/src/images/logo.png' alt='logo' />
+        <h1>Burning Space</h1>
+      </header>
       <Carrosel />
       {PROJECT.map((project) => (
         <ProjectCard key={project.id} {...project} />
