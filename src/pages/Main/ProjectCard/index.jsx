b158a52c360d618fb96110da88ace5e2055f6916
KGyeongSeo <loljyeo@gmail.com>
2025-01-04 22:11:36 +0900

    feat:프로젝트카드 컴포넌트 구현

--- src/pages/Main/ProjectCard/index.jsx
+++ src/pages/Main/ProjectCard/index.jsx
@@ -1,14 +1,36 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-export default function ProjectCard({ name }) {
+import styles from './index.module.css';
+
+export default function ProjectCard({ FrontTitle, BackTitle, subheading, Tayo, contents }) {
   return (
-    <div>
-      <h2>{name}</h2>
+    <div className={styles.container}>
+      <div className={styles.cardFront}>
+        <h2>{FrontTitle}</h2>
+        <h3>{subheading}</h3>
+        <img src={Tayo} />
+      </div>
+      <div className={styles.cardBack}>
+        <div className={styles.cardBackWrap}>
+          <h2 className={styles.titleC}>{BackTitle}</h2>
+          <h3>{subheading}</h3>
+          <p className={styles.contents}>{contents}</p>
+          <div className={styles.button1}>
+            <button>
+              <a href=''>더보기&gt;</a>
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
 
 ProjectCard.propTypes = {
-  name: PropTypes.string.isRequired,
+  FrontTitle: PropTypes.string.isRequired,
+  BackTitle: PropTypes.string.isRequired,
+  subheading: PropTypes.string.isRequired,
+  contents: PropTypes.string.isRequired,
+  Tayo: PropTypes.string.isRequired,
 };
